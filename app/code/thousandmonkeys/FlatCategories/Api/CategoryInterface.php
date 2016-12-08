8d925087ff92b4678f6ee7e0445e4c95b042ea4a
Chris Lingwood <chris@thousandmonkeys.com>
2016-12-08 22:22:41 +0000

    first commit

---
+++ app/code/thousandmonkeys/FlatCategories/Api/CategoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace thousandmonkeys\FlatCategories\Api;
+
+use thousandmonkeys\FlatCategories\Api\Data\PointInterface;
+
+interface CalculatorInterface
+{
+    public function getList();
+}
---
+++ app/code/thousandmonkeys/FlatCategories/Model/Category.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Copyright 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace thousandmonkeys\FlatCategories\Model;
+
+use thousandmonkeys\FlatCategories\Api\CategoryInterface;
+
+/**
+ * Defines the implementaiton class of the calculator service contract.
+ */
+class Category implements CategoryInterface
+{
+    protected $searchResultsFactory;
+    protected $searchCriteriaBuilder;
+    protected $collectionFactory;
+    protected $extensionAttributesJoinProcessor;
+
+	public function __construct(
+	\thousandmonkeys\FlatCategories\Api\CategorySearchResultsInterfaceFactory $searchResultsFactory,
+	\Magento\Framework\Api\SearchCriteriaBuilder $searchCriteriaBuilder,
+	\Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $collectionFactory,
+	\Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface $extensionAttributesJoinProcessor)
+	{
+		$this->searchResultsFactory = $searchResultsFactory;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+		$this->collectionFactory = $collectionFactory;
+        $this->extensionAttributesJoinProcessor = $extensionAttributesJoinProcessor;
+	}
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    {
+        /** @var \Magento\Catalog\Model\ResourceModel\Product\Collection $collection */
+        $collection = $this->collectionFactory->create();
+        $this->extensionAttributesJoinProcessor->process($collection);
+
+        foreach ($this->metadataService->getList($this->searchCriteriaBuilder->create())->getItems() as $metadata) {
+            $collection->addAttributeToSelect($metadata->getAttributeCode());
+        }
+
+        //Add filters from root filter group to the collection
+        foreach ($searchCriteria->getFilterGroups() as $group) {
+            $this->addFilterGroupToCollection($group, $collection);
+        }
+        /** @var SortOrder $sortOrder */
+        foreach ((array)$searchCriteria->getSortOrders() as $sortOrder) {
+            $field = $sortOrder->getField();
+            $collection->addOrder(
+                $field,
+                ($sortOrder->getDirection() == SortOrder::SORT_ASC) ? 'ASC' : 'DESC'
+            );
+        }
+        $collection->setCurPage($searchCriteria->getCurrentPage());
+        $collection->setPageSize($searchCriteria->getPageSize());
+        $collection->load();
+
+        $searchResult = $this->searchResultsFactory->create();
+        $searchResult->setSearchCriteria($searchCriteria);
+        $searchResult->setItems($collection->getItems());
+        $searchResult->setTotalCount($collection->getSize());
+        return $searchResult;
+    }
+
+        /**
+     * Helper function that adds a FilterGroup to the collection.
+     *
+     * @param \Magento\Framework\Api\Search\FilterGroup $filterGroup
+     * @param Collection $collection
+     * @return void
+     */
+    protected function addFilterGroupToCollection(
+        \Magento\Framework\Api\Search\FilterGroup $filterGroup,
+        Collection $collection
+    ) {
+        $fields = [];
+        $categoryFilter = [];
+        foreach ($filterGroup->getFilters() as $filter) {
+            $conditionType = $filter->getConditionType() ? $filter->getConditionType() : 'eq';
+
+            if ($filter->getField() == 'category_id') {
+                $categoryFilter[$conditionType][] = $filter->getValue();
+                continue;
+            }
+            $fields[] = ['attribute' => $filter->getField(), $conditionType => $filter->getValue()];
+        }
+
+        if ($categoryFilter) {
+            $collection->addCategoriesFilter($categoryFilter);
+        }
+
+        if ($fields) {
+            $collection->addFieldToFilter($fields);
+        }
+    }
+}
